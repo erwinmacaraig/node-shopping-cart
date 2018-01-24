4fe9bd2a5435d12987a5dab938f352cf331fe899
Erwin Malabanan Macaraig <Erwin.Macaraig@gbs.local>
2018-01-24 16:24:42 +0800

    worked on sign in users

--- config/passport.js
+++ config/passport.js
@@ -16,7 +16,17 @@ passport.use('local.signup', new LocalStrategy({
     usernameField: 'email',
     passwordField: 'password',
     passReqToCallback: true
-}, function(req, email, password, done){
+}, function(req, email, password, done) {
+    req.checkBody('email', 'Invalid email').notEmpty().isEmail();
+    req.checkBody('password', 'Invalid password').notEmpty().isLength({min: 4});
+    var errors = req.validationErrors();
+    if (errors) {
+        var messages = [];
+        errors.forEach(function(error){
+            messages.push(error.msg);
+        });
+        return done(null, false, req.flash('error', messages));
+    }
     User.findOne({'email': email}, function(err, user){
         if (err) {
             return done(err);
@@ -35,3 +45,35 @@ passport.use('local.signup', new LocalStrategy({
         });
     });
 }));
+passport.use('local.signin', new LocalStrategy({
+    usernamefield: 'email',
+    passwordField: 'password',
+    passReqToCallback: true
+}, function(req, email, password, done){
+    req.checkBody('email', 'Invalid email').notEmpty().isEmail();
+    req.checkBody('password', 'Invalid password').notEmpty();
+    var errors = req.validationErrors();
+    if (errors) {
+        var messages = [];
+        errors.forEach(function(error){
+            messages.push(error.msg);
+        });
+        return done(null, false, req.flash('error', messages));
+    }
+    User.findOne({'email': email}, function(err, user){
+        if (err) {
+            return done(err);
+        }
+        if (user) {
+            return done(null, false, {message: 'No user found.'});
+        }
+        if (!user.validPassword(password)) {
+            return disconnect(null, false, {message: 'Wrong password'});
+        }
+        var newUser = new User();
+        newUser.email = email;
+        newUser.password = newUser.encryptPassword(password);
+        return done(null, user);
+    });
+}));
+
